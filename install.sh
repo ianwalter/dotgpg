4648a1d15191ceedf646214e7771800b1e1ce72e
Ian Walter <public@iankwalter.com>
2019-11-27 15:12:35 -0500

    Importing keys from Keybase

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 mkdir -p ~/.gnupg
 cp gpg.conf gpg-agent.conf ~/.gnupg
 
+# Import the public key from Keybase.
+keybase pgp export | gpg --import
+
+# Import the private key from Keybase.
+keybase pgp export -s | gpg --allow-secret-key-import --import
+
 if [[ $? == 0 ]]; then
   printf "\n✅ Installed GPG configuration successfully.\n"
 fi
